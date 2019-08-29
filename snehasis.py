b88ce0ca9039f9e6a9538de9715a1bf3b8f99c0c
Snehasis-oss <54390795+Snehasis-oss@users.noreply.github.com>
2019-08-29 12:15:36 +0530

    Add files via upload

--- snehasis.py
+++ snehasis.py
@@ -1,3 +1,19 @@
-list1=[1,2,3,4,5,6,7,8,9,10]
-even_list=[i for i in list1 if i%2==0]
-print (even_list)
+def search1(list1,n):
+    if n in list1:
+        return (1)
+    else:
+        return (0)
+n=int(input("enter the no of elements in the list\n"))
+list2=[]
+no=int(input("enter the no to be searched\n"))
+print ("enter elements")     
+for i in range(0,n):
+      a=int(input())
+      list2.append(a)
+result=search1(list2,no)
+if result==True:
+      print("entered element ",(no)," is present")
+else:
+      print("entered element ",(no)," is not present")
+            
+            
